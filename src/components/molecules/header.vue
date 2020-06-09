84d82dfcebc30395716203814f2b9e9ec9219f4d
rikutkb <iwasaki.riku.td@alumni.tsukuba.ac.jp>
2020-06-09 13:03:30 +0900

    他の記述を追加

--- src/components/molecules/header.vue
+++ src/components/molecules/header.vue
@@ -7,18 +7,8 @@
     <b-collapse id="nav-collapse" is-nav>
       <b-navbar-nav>
         <b-nav-item to="/Works">Works</b-nav-item>
-        <b-nav-item to="/About">About</b-nav-item>
-        <b-nav-item to="/Profile">Profile</b-nav-item>
       </b-navbar-nav>
 
-      <!-- Right aligned nav items -->
-      <b-navbar-nav class="ml-auto">
-        <b-nav-form>
-          <b-form-input size="sm" class="mr-sm-2" placeholder="Search"></b-form-input>
-          <b-button size="sm" class="my-2 my-sm-0" type="submit">Search</b-button>
-        </b-nav-form>
-
-      </b-navbar-nav>
     </b-collapse>
   </b-navbar>
 </div></template>
--- src/components/templates/Top.vue
+++ src/components/templates/Top.vue
@@ -1,5 +1,37 @@
 <template>
   <div>
+    <b-card
+      title="自己紹介"
+      tag="article"
+      class="mb-12"
+    >
+      <b-card-text>
+        2016年4月:筑波大学情報メディア創成学類入学<br>
+        2020年3月:卒業<br>
+        2020年4月筑波大学院情報工学研究群情報理工学位プログラム入学<br>
+        2022年3月卒業予定<br>
+        コンピュータネットワーク研究室所属<br>
+      </b-card-text>
+    </b-card>
+    <b-card
+      title="プログラミング言語"
+      tag="article"
+      class="mb-12"
+    >
+      <b-card-text>
+        C++,C#,javascript
+      </b-card-text>
+    </b-card>
+    <b-card
+      title="social"
+      tag="article"
+      class="mb-12"
+    >
+      <b-card-text>
+        <b-button href="https://github.com/rikutkb" variant="primary">GITHUB</b-button>
+        <b-button href="https://atcoder.jp/users/iwasaki_tkb?lang=ja" variant="primary">Atcoder</b-button>
+      </b-card-text>
+    </b-card>
   </div>
 </template>
 <script>
@@ -25,8 +57,5 @@ ul {
 li {
   display: inline-block;
   margin: 0 10px;
-}
-a {
-  color: #42b983;
 }
 </style>
