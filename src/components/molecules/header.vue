0974a9b8ffc2a43893d2d765e84a05d3c07bfc92
rikutkb <iwasaki.riku.td@alumni.tsukuba.ac.jp>
2020-06-10 15:10:34 +0900

    TOPのパス修正

--- src/components/molecules/header.vue
+++ src/components/molecules/header.vue
@@ -1,7 +1,7 @@
 <template>
 <div>
   <b-navbar toggleable="lg" type="dark" variant="dark">
-    <b-navbar-brand href="/">TOP</b-navbar-brand>
+    <b-navbar-brand to="/">TOP</b-navbar-brand>
 
     <b-navbar-toggle target="nav-collapse"></b-navbar-toggle>
     <b-collapse id="nav-collapse" is-nav>
